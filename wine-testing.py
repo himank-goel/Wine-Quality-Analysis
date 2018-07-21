3328fe1a5b0400580b50d7497c707cf8f49d46fd
Himank Goel <himankgoel03@gmail.com>
2018-07-21 13:26:51 +0530

    Accuracy Function

--- wine-testing.py
+++ wine-testing.py
@@ -9,6 +9,19 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.externals import joblib
 
+# Function for accuracy
+
+
+def accuracy(Y_test, Y_pred):
+    equal = 0
+    for i in xrange(len(Y_pred)):
+        if round(Y_pred[i]) == Y_test[i]:
+            equal += 1
+        # print Y_test[i]
+
+    print 'Accuracy = %s' % (float(equal)/len(Y_pred))
+
+
 # Loading the dataset
 dataset_url = 'http://mlr.cs.umass.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv'
 data = pd.read_csv(dataset_url, sep=';')
@@ -23,19 +36,21 @@ X_train, X_test, Y_train, Y_test = model_selection.train_test_split(
 pipeline = make_pipeline(preprocessing.StandardScaler(),
                          RandomForestRegressor(n_estimators=100))
 
-#Setting the HyperParameters
+# Setting the HyperParameters
 hyperparameters = {'randomforestregressor__max_features': [
     'auto', 'sqrt', 'log2'], 'randomforestregressor__max_depth': [None, 5, 3, 1]}
 
-#Fitting the classfier
+# Fitting the classfier
 clf = GridSearchCV(pipeline, hyperparameters, cv=10)
 clf.fit(X_train, Y_train)
 
-#Making prediction on the test set
+# Making prediction on the test set
 Y_pred = clf.predict(X_test)
 
 print r2_score(Y_test, Y_pred)
 print mean_squared_error(Y_test, Y_pred)
 
-#Saving it for further use
+accuracy(Y_test.tolist(), Y_pred)
+
+# Saving it for further use
 joblib.dump(clf, 'rf_regressor.pkl')
